0356da095c732611617e0070769e794db7b2f5f0
김동규 <dgorzldd@gmail.com>
2025-02-16 03:25:05 +0900

    Bottom Navigation 2 - Custom

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -2,14 +2,41 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { Text, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Search"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "black",
+        },
+        tabBarLabelStyle: {
+          backgroundColor: "red",
+        },
+        tabBarLabelPosition: "beside-icon",
+        // headerShown: false,
+        headerRight: () => (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        ),
+      }}
+    >
       <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen
+        name="Tv"
+        component={Tv}
+        options={{
+          tabBarLabelStyle: {
+            backgroundColor: "purple",
+          },
+          tabBarBadge: 5,
+        }}
+      />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
   );
