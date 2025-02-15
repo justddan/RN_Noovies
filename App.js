53854325e0dbd4f7e1926c9068c06bae165cc574
김동규 <dgorzldd@gmail.com>
2025-02-16 02:25:22 +0900

    npx create-expo-app --template blank 프로젝트명 / Splash Screen

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // pre-load fonts, call APIs, etc
+        // 강의의 startLoading과 동일하게 동작
+        await Font.loadAsync(Ionicons.font);
+        await Asset.loadAsync(require("./my-face.jpeg"));
+      } catch (error) {
+        // 강의의 onError와 동일하게 동작
+        console.warn(error);
+      } finally {
+        // 강의의 onFinish와 동일하게 동작
+        setReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) await SplashScreen.hideAsync();
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView}>
+      <Text>We are done Loading !</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
