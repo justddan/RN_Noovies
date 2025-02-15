1bd5c739779e287cf0a218ab0d38353e7300d95d
김동규 <dgorzldd@gmail.com>
2025-02-16 02:34:37 +0900

    useAsset / useFonts

--- App.js
+++ App.js
@@ -2,38 +2,65 @@ import React, { useCallback, useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Text, View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
+// const loadFonts = (fonts) => {
+//   return fonts.map((font) => Font.loadAsync(font));
+// };
+
+// const loadImages = (images) => {
+//   return images.map((image) => {
+//     if (typeof image === "string") {
+//       return Image.prefetch(image);
+//     } else {
+//       return Asset.loadAsync(image);
+//     }
+//   });
+// };
+
 export default function App() {
-  const [ready, setReady] = useState(false);
+  // const [ready, setReady] = useState(false);
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // pre-load fonts, call APIs, etc
-        // 강의의 startLoading과 동일하게 동작
-        await Font.loadAsync(Ionicons.font);
-        await Asset.loadAsync(require("./my-face.jpeg"));
-      } catch (error) {
-        // 강의의 onError와 동일하게 동작
-        console.warn(error);
-      } finally {
-        // 강의의 onFinish와 동일하게 동작
-        setReady(true);
-      }
-    }
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       // pre-load fonts, call APIs, etc
+  //       // 강의의 startLoading과 동일하게 동작
+  //       const fonts = loadFonts([Ionicons.font]);
+  //       const images = loadAssets([
+  //         require("./my-face.jpeg"),
+  //         "https://picsum.photos/200/300",
+  //       ]);
+  //       await Promise.all([...fonts, ...images]);
+  //     } catch (error) {
+  //       // 강의의 onError와 동일하게 동작
+  //       console.warn(error);
+  //     } finally {
+  //       // 강의의 onFinish와 동일하게 동작
+  //       setReady(true);
+  //     }
+  //   }
 
-    prepare();
-  }, []);
+  //   prepare();
+  // }, []);
+
+  const [fontsLoaded] = Font.useFonts({
+    Ionicons: Ionicons.font,
+  });
+
+  const [assets] = useAssets([
+    require("./my-face.jpeg"),
+    "https://picsum.photos/200/300",
+  ]);
 
   const onLayoutRootView = useCallback(async () => {
-    if (ready) await SplashScreen.hideAsync();
-  }, [ready]);
+    if (fontsLoaded && assets) await SplashScreen.hideAsync();
+  }, [fontsLoaded, assets]);
 
-  if (!ready) {
+  if (!fontsLoaded || !assets) {
     return null;
   }
 
