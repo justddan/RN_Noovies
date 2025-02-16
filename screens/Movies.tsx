2d010503f503e849196a217dd05ba99458a62f7c
김동규 <dgorzldd@gmail.com>
2025-02-16 21:27:57 +0900

    가로 스크롤뷰 만들기

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
-import { Dimensions } from "react-native";
+import { Dimensions, ScrollView } from "react-native";
 import { ActivityIndicator } from "react-native";
 import Slide from "../components/Slide";
+import Poster from "../components/Poster";
 
 const API_KEY = "fec442424b0d4ae084e35e7b48cab4c6";
 
@@ -17,6 +18,32 @@ const Loader = styled.View`
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }: { theme: any }) => theme.mainBgColor};
+`;
+
+const ListTitle = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+
+const Votes = styled.Text`
+  font-size: 10px;
+  font-weight: 600;
 `;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -76,7 +103,11 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({}) => {
         horizontal
         loop
         autoplay
-        containerStyle={{ width: "100%", height: SCREEN_HEIGHT * 0.25 }}
+        containerStyle={{
+          width: "100%",
+          height: SCREEN_HEIGHT * 0.25,
+          marginBottom: 30,
+        }}
         showsButtons={false}
         autoplayTimeout={timeout}
         showsPagination={false}
@@ -92,6 +123,23 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({}) => {
           />
         ))}
       </Swiper>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingLeft: 30 }}
+      >
+        {trending.map((movie: any) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? "..." : null}
+            </Title>
+            <Votes>⭐️ {movie.vote_average} / 10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
